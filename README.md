0922494020b5db2d5dd4094a76b207131c11d28f
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2024-03-20 16:05:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# How to use linters
+## Description
+This project is about how to use linters in our code.
+## Built With
+- PHP 8.2
+- Composer
+- Linters
+- 
+## Getting Started
+To get a local copy up and running follow these simple example steps.
+- Clone the project with `git clone
+- `cd` into the project directory
+- refactor the files in src folder
+- Run `composer install`
+- install phploc
+- install phpmd
+- install phpcs
---
+++ edit_post.php
@@ -0,0 +1,37 @@
+<?php
+include 'functions.php';
+if (!isset($_GET['id'])) {
+    echo "L'article n'est pas spécifié.";
+    exit;
+}
+$post = getPostById($_GET['id']);
+if (!$post) {
+    echo "L'article demandé n'a pas été trouvé.";
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Éditer Article</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include 'includes/nav.php'; ?>
+<div class="container">
+    <h1>Éditer l'article</h1>
+    <form action="update_post.php" method="POST">
+        <input type="hidden" name="id" value="<?php echo $post['id']; ?>">
+
+        <label for="title">Titre de l'article :</label>
+        <input type="text" id="title" name="title" value="<?php echo htmlspecialchars($post['title'], ENT_QUOTES); ?>" required>
+
+        <label for="content">Contenu :</label>
+        <textarea id="content" name="content" required><?php echo htmlspecialchars($post['content'], ENT_QUOTES); ?></textarea>
+
+        <button type="submit">Mettre à jour</button>
+    </form>
+</div>
+</body>
+</html>
